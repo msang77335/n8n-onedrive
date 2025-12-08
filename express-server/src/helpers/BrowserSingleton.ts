40efeb6f2878d7b64de9a243ca350f3df7720f79
msang77335 <msang77335@gmail.com>
2025-12-08 17:13:04 +0700

    Update docker compose file

--- express-server/src/helpers/BrowserSingleton.ts
+++ express-server/src/helpers/BrowserSingleton.ts
@@ -10,19 +10,21 @@ export class BrowserSingleton {
       return this.browserInstance;
     }
     console.log('🆕 [BROWSER] Creating new browser instance');
-    this.browserInstance = await puppeteer.launch({
-      headless: true,
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-      args: [
-        '--no-sandbox',
-        // 2. Các cờ hỗ trợ Evasion (Né tránh Bot)
-        '--no-first-run',
-        '--no-default-browser-check',
-        '--disable-site-isolation-trials',
-        '--disable-web-security',
-        '--single-process' // Giảm thiểu phát hiện
-      ]
-    });
+    // this.browserInstance = await puppeteer.launch({
+    //   headless: true,
+    //   executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    //   args: [
+    //     '--no-sandbox',
+    //     // 2. Các cờ hỗ trợ Evasion (Né tránh Bot)
+    //     '--no-first-run',
+    //     '--no-default-browser-check',
+    //     '--disable-site-isolation-trials',
+    //     '--disable-web-security',
+    //     '--single-process' // Giảm thiểu phát hiện
+    //   ]
+    // });
+    const pwEndpoint = `ws://headless-chrome:${process.env.BROWSERLESS_PORT}?token=${process.env.BROWSERLESS_API_TOKEN_LOCAL}`;
+    this.browserInstance = await puppeteer.connect({ browserWSEndpoint: pwEndpoint });
     this.browserInstance.on('disconnected', () => {
       console.log('🔌 [BROWSER] Browser disconnected');
       this.browserInstance = null;
