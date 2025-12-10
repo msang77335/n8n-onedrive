18543f2cf0941284490a2e941728022c4c574d70
msang77335 <msang77335@gmail.com>
2025-12-10 16:20:08 +0700

    Update code

--- express-server/src/helpers/BrowserSingleton.ts
+++ express-server/src/helpers/BrowserSingleton.ts
@@ -1,18 +1,15 @@
 import type { Browser } from 'puppeteer';
 import puppeteer from 'puppeteer-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import RecaptchaPlugin from 'puppeteer-extra-plugin-recaptcha';
-
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 export class BrowserSingleton {
   private static browserInstance: Browser | null = null;
 
   static async getInstance(): Promise<Browser> {
-    if (this.browserInstance) {
+    if (this.browserInstance?.wsEndpoint()) {
       console.log('♻️ [BROWSER] Reusing existing browser instance');
       return this.browserInstance;
     }
-    console.log('🆕 [BROWSER] Creating new browser instance');
-
     // Configure plugins
     puppeteer.use(StealthPlugin());
     puppeteer.use(
@@ -24,12 +21,22 @@ export class BrowserSingleton {
         visualFeedback: true,
       })
     );
-    const pwEndpoint = `ws://headless-chrome:${process.env.BROWSERLESS_PORT}?token=${process.env.BROWSERLESS_API_TOKEN_LOCAL}`;
-    this.browserInstance = await puppeteer.connect({ browserWSEndpoint: pwEndpoint });
-    this.browserInstance.on('disconnected', () => {
-      console.log('🔌 [BROWSER] Browser disconnected');
-      this.browserInstance = null;
+    console.log('🆕 [BROWSER] Creating new browser instance');
+    this.browserInstance = await puppeteer.launch({
+      headless: true,
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+      args: [
+        '--no-sandbox',
+        // 2. Các cờ hỗ trợ Evasion (Né tránh Bot)
+        '--no-first-run',
+        '--no-default-browser-check',
+        '--disable-site-isolation-trials',
+        '--disable-web-security',
+        '--single-process' // Giảm thiểu phát hiện
+      ]
     });
+    // const pwEndpoint = `ws://headless-chrome:${process.env.BROWSERLESS_PORT}?token=${process.env.BROWSERLESS_API_TOKEN_LOCAL}`;
+    // this.browserInstance = await puppeteer.connect({ browserWSEndpoint: pwEndpoint });
     this.browserInstance.on('disconnected', () => {
       console.log('🔌 [BROWSER] Browser disconnected');
       this.browserInstance = null;
