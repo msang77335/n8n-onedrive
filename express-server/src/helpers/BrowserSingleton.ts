4efae0c39ac235ae0116972112dbc373fcd7ab6d
msang77335 <msang77335@gmail.com>
2025-12-10 16:13:50 +0700

    Update code

--- express-server/src/helpers/BrowserSingleton.ts
+++ express-server/src/helpers/BrowserSingleton.ts
@@ -24,7 +24,7 @@ export class BrowserSingleton {
         visualFeedback: true,
       })
     );
-    const pwEndpoint = `ws://headless-chrome:${process.env.BROWSERLESS_PORT}?token=${process.env.BROWSERLESS_API_TOKEN}`;
+    const pwEndpoint = `ws://headless-chrome:${process.env.BROWSERLESS_PORT}?token=${process.env.BROWSERLESS_API_TOKEN_LOCAL}`;
     this.browserInstance = await puppeteer.connect({ browserWSEndpoint: pwEndpoint });
     this.browserInstance.on('disconnected', () => {
       console.log('🔌 [BROWSER] Browser disconnected');
