b7706dd69e131fa7cb765d52c13fac3e7e25e49d
msang77335 <msang77335@gmail.com>
2025-12-22 17:06:37 +0700

    Update

--- express-server/src/routes/index.ts
+++ express-server/src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Request, Response, Router } from 'express';
 import screenshotRoutes from './screenshotRoutes';
 import viettelTrackingRoutes from './viettelTrackingRoutes';
+import checkShopRoutes from './checkShop';
 
 const router = Router();
 
 // Mount route handlers
 router.use('/screenshot', screenshotRoutes);
 router.use('/viettel-tracking', viettelTrackingRoutes);
+router.use('/check-shop', checkShopRoutes);
 
 // Default API route
 router.get('/', (req: Request, res: Response): void => {
@@ -17,6 +19,7 @@ router.get('/', (req: Request, res: Response): void => {
       health: '/health',
       screenshot: '/api/v1/screenshot',
       viettelTracking: '/api/v1/viettel-tracking',
+      checkShop: '/api/v1/check-shop',
     }
   });
 });
