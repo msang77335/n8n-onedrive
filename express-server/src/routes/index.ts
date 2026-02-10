12e437edf5212a82b3b98a85c65a68942eeb67f1
msang77335 <msang77335@gmail.com>
2026-02-10 10:08:19 +0700

    Remove checkShopRoutes

--- express-server/src/routes/index.ts
+++ express-server/src/routes/index.ts
@@ -1,14 +1,12 @@
 import { Request, Response, Router } from 'express';
 import screenshotRoutes from './screenshotRoutes';
 import viettelTrackingRoutes from './viettelTrackingRoutes';
-import checkShopRoutes from './checkShop';
 
 const router = Router();
 
 // Mount route handlers
 router.use('/screenshot', screenshotRoutes);
 router.use('/viettel-tracking', viettelTrackingRoutes);
-router.use('/check-shop', checkShopRoutes);
 
 // Default API route
 router.get('/', (req: Request, res: Response): void => {
@@ -19,7 +17,6 @@ router.get('/', (req: Request, res: Response): void => {
       health: '/health',
       screenshot: '/api/v1/screenshot',
       viettelTracking: '/api/v1/viettel-tracking',
-      checkShop: '/api/v1/check-shop',
     }
   });
 });
